024f04741555a777f8fd708c19c96f2ba82d5170
Saima Khan <tithi888khan@gmail.com>
2021-01-27 19:52:36 +0600

    Generator get values and error

--- src/App.js
+++ src/App.js
@@ -5,10 +5,40 @@ import SingleColor from './SingleColor'
 import Values from 'values.js';
 
 function App() {
-  return (
-    <div className="container">
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState ([]);
 
-    </div>
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  try{
+    let colors = new Values(color).all(10)
+    setList(colors)
+  } catch (error){
+    setError (true)
+    console.log(error)
+  }
+}  
+  return (
+    <>
+    <section className="container">
+    <h2>Color generator</h2>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder="#f15025" className={`${error ? 'error' : null }`}
+
+      />
+      <button className="btn" type="submit">submit</button>
+    </form>
+
+    </section>
+    <section className="colors">
+      {list.map((color,index)=>{
+        console.log(color)
+        return <SingleColor key={index} {...color} index= {index} />
+      })}
+    </section>
+    </>
   )
 }
 
