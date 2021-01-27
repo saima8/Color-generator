bcb6418d28524bd265a51a461d20177d161c7295
Saima Khan <tithi888khan@gmail.com>
2021-01-27 20:25:59 +0600

    added color percentage

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import rgbtoHex from './utils'
 
-const SingleColor = () => {
-    return <h4>Single color</h4>
+const SingleColor = ({rgb, weight, index, hexcolor}) => {
+    const [alert, setAlert] = useState(false)
+    const bcg = rgb.join(',')
+    const hex = rgbtoHex(...rgb);
+    return (
+        <article className={`color`} style={{backgroundColor: `rgb({${bcg}})`}}>
+            <p className="percent-value">{weight}%</p>
+            <p className="color-value">{hex}</p>
+        </article>
+        
+    )
 }
 
 export default SingleColor
